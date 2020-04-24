62dc7c50653a98a8d0c053bcd3553597a492811d
Arif Rabbani <arif.93rabbani@gmail.com>
2020-04-24 22:07:00 +0530

    AppComponent cleanu. Pizza list module added

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-pizza'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-pizza');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('my-pizza app is running!');
-  });
 });
---
+++ src/app/pizza-list/pizza-list.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PizzaListRoutingModule } from './pizza-list-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PizzaListRoutingModule
+  ]
+})
+export class PizzaListModule { }

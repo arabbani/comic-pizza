37d05ecc504aaaa9c3789077a37c41d7477f4414
Arif Rabbani <arif.93rabbani@gmail.com>
2020-04-25 13:44:46 +0530

    css in progress

--- src/app/pizza-list/pizza-list.module.ts
+++ src/app/pizza-list/pizza-list.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PizzaListRoutingModule } from './pizza-list-routing.module';
+import { PizzaListComponent } from './pizza-list/pizza-list.component';
+import { PizzaFilterComponent } from './pizza-filter/pizza-filter.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [PizzaListComponent, PizzaFilterComponent],
   imports: [
     CommonModule,
     PizzaListRoutingModule
